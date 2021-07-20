8f26cdd6e6f8ee8b436d19080a4206da6fc0cada
theBoySamm <onyemasamuel96@gmail.com>
2021-07-20 02:38:30 +0100

    add a path  'posts' and required it in app.js

---
+++ posts.js
@@ -0,0 +1,74 @@
+const posts = [
+  {
+    imageUrl:
+      "https://cdn.joinhoney.com/images/honey-blog/press/business-insider.jpg",
+    postHeader:
+      "These 13 Google Chrome Hacks Will Change the Way You Use the Internet",
+    postBody:
+      "Just click the Honey button and it will scour the web and apply coupon codes to your shopping cart.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/nytimes.jpg",
+    postHeader: "8 Things You Can Do Now to Save Money on Travel",
+    postBody:
+      "Install the Honey browser extension, which works on several prominent travel sites.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/time.jpg",
+    postHeader: "13 Google Chrome Tips That Will Make Your Life Better",
+    postBody: "It's basically just free money.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/teenvogue.jpg",
+    postHeader: "How to Save Money on Your Fall Wardrobe",
+    postBody:
+      "Use the Honey plugin to easily search for promo codes at checkout.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/glamour.jpg",
+    postHeader: "The Online Shopping Hack All Beauty Junkies Need to Know",
+    postBody:
+      "Amazing. Never spend time searching Google for coupon codes ever again.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/instyle.jpg",
+    postHeader:
+      "Never Pay Retail Price Again with These Shopping Apps (As Seen on the Today Show)",
+    postBody:
+      "Honey will scour the web for discount codes and try multiple ones in just a few seconds.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/forbes.jpg",
+    postHeader: "7 Shopping Tips to Give Big and Stay Rich",
+    postBody:
+      "Honey will alert you to any coupons and deals available on the sites you browse.",
+  },
+  {
+    imageUrl:
+      "https://cdn.joinhoney.com/images/honey-blog/press/refinery29.jpg",
+    postHeader: "How to Score the Best Deals While Online Shopping",
+    postBody:
+      "Honey automatically applies available coupon codes to your shopping cart.",
+  },
+  {
+    imageUrl:
+      "https://cdn.joinhoney.com/images/honey-blog/press/who-what-wear.jpg",
+    postHeader: "This Online Shopping Hack Will Save You Tons of Money",
+    postBody:
+      "Honey scanned the sites and tested possible codes that ended up leading to some surprising discounts.",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/cnet.jpg",
+    postHeader: "Use Honey to Save Money on Amazon Purchases",
+    postBody: "Seems like a no-brainer add-on to me!",
+  },
+  {
+    imageUrl: "https://cdn.joinhoney.com/images/honey-blog/press/buzzfeed.jpg",
+    postHeader:
+      "This Lazy Online Shopping Trick Actually Saves You a Ton of Money",
+    postBody:
+      "This Lazy Online Shopping Trick Actually Saves You a Ton of Money",
+  },
+];
+
+module.exports = posts
